56a76c9ca71211741c2e4a9ca1ba3a0adedf7683
Rajaa <rajaa.elmorabet@gmail.com>
2024-05-04 02:52:36 +0000

    zombie

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Infinite loop
+ *
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+    pid_t zombie_pid;
+
+    for (i = 0; i < 5; i++)
+    {
+        zombie_pid = fork();
+        if (zombie_pid == 0)
+            exit(0);
+        else
+            printf("Zombie process created, PID: %d\n", zombie_pid);
+    }
+
+    infinite_while();
+    return (0);
+}
